c6bf3b0849a447ed1d7dd0fb6123a668294f579c
XOrfeo <dmgardner13@gmail.com>
2025-09-11 20:10:50 +0100

    Added Decimal <=> Hexadecimal conversion scripts

---
+++ hexToDec.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+
+inputNumber = sys.argv[1]
+
+digitConvert = { '0':0 , '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15 }
+
+reverseInput = inputNumber[::-1]
+
+exponent = 0
+decimalNumber = 0
+for character in reverseInput:
+    decimalNumber = decimalNumber + (digitConvert[character] * (16 ** exponent))
+    exponent = exponent + 1
+
+print(decimalNumber)
