bc96d6d97a842b0f8e8878c6904bb1921df34434
Dom <dmgardner3@protonmail.com>
2026-01-15 17:41:29 +0000

    Added comment to naas.sh

--- naas.sh
+++ naas.sh
@@ -1,1 +1,2 @@
+# Pulls a rejection reason from the No As A Service (NAAS) API and pastes it to the terminal & clipboard
 echo $(curl -s https://naas.isalman.dev/no | jq -r .reason | tee >(wl-copy))
