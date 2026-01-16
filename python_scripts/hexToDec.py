b18f60d730a13c8523d9e36f5e18872afce3ee51
Alf <dmgardner3@protonmail.com>
2026-01-16 12:05:46 +0000

    Update hexToDec.py
    more efficiency

--- python_scripts/hexToDec.py
+++ python_scripts/hexToDec.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 import sys
 
-reverseInput = sys.argv[1][::-1]
-
 digitConvert = { '0':0 , '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15 }
 
 exponent , decimal number = 0 , 0
-for character in reverseInput:
+for character in sys.argv[1][::-1]:
     decimalNumber = decimalNumber + (digitConvert[character] * (16 ** exponent))
     exponent = exponent + 1
 
